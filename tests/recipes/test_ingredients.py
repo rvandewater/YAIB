792d7d2cad5107ac544ffdd0ab210f190860f2b2
prockenschaub <rockenschaub.patrick@gmail.com>
2022-09-26 10:57:46 +0200

    Add tests for role copying behaviour

--- tests/recipes/test_ingredients.py
+++ tests/recipes/test_ingredients.py
@@ -21,6 +21,30 @@ def test_init_role_typo(example_df):
         Ingredients(example_df, roles={'z': 'outcome'})
     assert e_info.match('variable name that is not in the data')
 
+def test_init_role_copy(example_df):
+    roles = {'y': ['outcome']}
+    ing = Ingredients(example_df, roles=roles)
+    roles['x1'] = ['predictor']
+    assert ing.roles == {'y': ['outcome']}
+
+def test_init_role_noncopy(example_df):
+    roles = {'y': ['outcome']}
+    ing = Ingredients(example_df, copy=False, roles=roles)
+    roles['x1'] = ['predictor']
+    assert ing.roles == {'x1': ['predictor'], 'y': ['outcome']}
+
+def test_reinit_copy(example_df):
+    ing = Ingredients(example_df, roles={'y': ['outcome']})
+    reing = Ingredients(ing)
+    reing.add_role('y', 'predictor')
+    assert ing.roles != reing.roles
+
+def test_reinit_noncopy(example_df):
+    ing = Ingredients(example_df, roles={'y': ['outcome']})
+    reing = Ingredients(ing, copy=False)
+    reing.add_role('y', 'predictor')
+    assert ing.roles == reing.roles
+
 def test_add_role(example_ingredients):
     example_ingredients.update_role('y', 'first role')
     example_ingredients.add_role('y', 'another role')
