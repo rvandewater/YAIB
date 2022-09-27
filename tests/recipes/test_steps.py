f0b71609a0b35c338a4fc38ba95fe38a91221417
Hendrik Schmidt <hendriknas@gmail.com>
2022-09-27 18:48:49 +0200

    add tests for scalers

--- tests/recipes/test_steps.py
+++ tests/recipes/test_steps.py
@@ -18,3 +18,30 @@ class TestSklearnStep:
         rec.add_step(StepSklearn(sel=all_numeric_predictors(), sklearn_transform=SimpleImputer(strategy='constant', fill_value=0)))
         df = rec.prep()
         assert (df.loc[[1,2,4,7], 'x1'] == 0).all()
+
+    def test_standard_scaler(self, example_df):
+        rec = Recipe(example_df, ['y'], ['x1', 'x2'], ['id']) # FIXME: add squence when merged
+        rec.add_step(StepSklearn(sel=all_numeric_predictors(), sklearn_transform=StandardScaler()))
+        df = rec.prep()
+        assert abs(df['x1'].mean()) < 0.00001
+        assert abs(df['x2'].mean()) < 0.00001
+
+    def test_min_max_scaler(self, example_df):
+        rec = Recipe(example_df, ['y'], ['x1', 'x2'], ['id']) # FIXME: add squence when merged
+        rec.add_step(StepSklearn(sel=all_numeric_predictors(), sklearn_transform=MinMaxScaler()))
+        df = rec.prep()
+        assert ((0 <= df['x1']) & (df['x1'] <= 1)).all()
+        assert ((0 <= df['x2']) & (df['x2'] <= 1)).all()
+
+    def test_max_abs_scaler(self, example_df):
+        rec = Recipe(example_df, ['y'], ['x1', 'x2'], ['id']) # FIXME: add squence when merged
+        rec.add_step(StepSklearn(sel=all_numeric_predictors(), sklearn_transform=MaxAbsScaler()))
+        df = rec.prep()
+        assert ((-1 <= df['x1']) & (df['x1'] <= 1)).all()
+        assert ((-1 <= df['x2']) & (df['x2'] <= 1)).all()
+
+    def test_robust_scaler(self, example_df):
+        rec = Recipe(example_df, ['y'], ['x1', 'x2'], ['id']) # FIXME: add squence when merged
+        rec.add_step(StepSklearn(sel=all_numeric_predictors(), sklearn_transform=RobustScaler()))
+        df = rec.prep()
+        assert df['x1'].median() == 0
