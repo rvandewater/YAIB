fc5eb1ba2d6e08e47243869eff1c7f9755e83016
Hendrik Schmidt <hendriknas@gmail.com>
2022-10-28 20:33:08 +0200

    make program parse arbitrary params from CLI

--- icu_benchmarks/models/utils.py
+++ icu_benchmarks/models/utils.py
@@ -29,11 +29,9 @@ def save_config_file(log_dir):
 
 
 @gin.configurable("bindings")
-def get_bindings(cli_params, args, log_dir, do_rs=False, **rs_params_from_config):
-    # only handle cli params that are set (exist in args and aren't None)
-    cli_params = {param: getattr(args, param) for param in cli_params if getattr(args, param, None) is not None}
+def get_bindings(hyperparams, log_dir, do_rs=False, **rs_params_from_config):
     # merge params for random search from config with cli params (cli overwrites config)
-    merged_params = rs_params_from_config | cli_params
+    merged_params = rs_params_from_config | hyperparams
     gin_bindings = []
     for name, params in merged_params.items():
         # randomly choose one param from list if random search enable, else take first
--- icu_benchmarks/run.py
+++ icu_benchmarks/run.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import argparse
+from argparse import BooleanOptionalAction
+from ast import literal_eval
 import logging
 import sys
 from pathlib import Path
@@ -10,30 +12,8 @@ from icu_benchmarks.data.preprocess import preprocess_data
 from icu_benchmarks.models.train import train_with_gin
 from icu_benchmarks.models.utils import get_bindings
 
-max_rs_attempt = 300
-default_seeds = [1111]
-cli_hyper_params = [
-    ("bs", "batch_size", int, None, "Batchsize for the model"),
-    ("lr", "learning_rate", float, None, "Learning rate for the model"),
-    ("nc", "num_class", int, None, "Number of classes considered for the task"),
-    ("emb", "embeddings", int, None, "Embedding size of the input data"),
-    ("k", "kernel_size", int, None, "Kernel size for Temporal CNN"),
-    ("hi", "hidden", int, None, "Dimensionality of hidden layer in Neural Networks"),
-    ("de", "depth", int, None, "Number of layers in Neural Network"),
-    ("ho", "horizon", int, None, "History length for Neural Networks"),
-    ("do", "drop_out", float, None, "Dropout probability"),
-    ("do_att", "drop_out_att", float, None, "Dropout probability for the Self-Attention layer only"),
-    ("he", "heads", int, None, "Number of heads in Sel-Attention layer"),
-    ("la", "latent", int, None, "Dimension of fully-conected layer in Transformer block"),
-    ("ssd", "subsample_data", float, None, "Parameter in Gradient Boosting, subsample ratio of the training"),
-    ("ssf", "subsample_feat", float, None, "Subsample ratio of columns when constructing each tree in LGBM"),
-    ("reg", "l1_reg", float, None, "L1 regularization coefficient for Transformer"),
-    ("cp", "c_parameter", float, None, "C parameter in Logistic Regression"),
-    ("pen", "penalty", float, None, "Penalty parameter for Logistic Regression"),
-    ("lw", "loss_weight", str, None, "Loss weigthing parameter"),
-    # ("r", "resampling", int, None, "Resampling for the data"),
-    # ("rl", "resampling_label", int, None, "Resampling for the prediction"),
-]
+MAX_ATTEMPTS = 300
+SEEDS = [1111]
 
 
 def build_parser():
@@ -44,52 +24,41 @@ def build_parser():
     parent_parser = argparse.ArgumentParser(add_help=False)
     subparsers = parser.add_subparsers(title="Commands", dest="command", required=True)
 
-    #
     # ARGUMENTS FOR ALL COMMANDS
-    #
     general_args = parent_parser.add_argument_group("General arguments")
-    general_args.add_argument(
-        "-dir", "--data-dir", required=True, dest="data_dir", type=Path, help="Path to the parquet data directory."
-    )
-    general_args.add_argument("-t", "--task", default="Mortality_At24Hours", dest="task_config", help="Name of the task gin.")
-    general_args.add_argument("-m", "--model", default="LGBMClassifier", dest="model_config", help="Name of the model gin.")
-    general_args.add_argument("-e", "--experiment", dest="experiment_config", help="Name of the experiment gin.")
-    general_args.add_argument(
-        "-l", "--log-dir", dest="log_dir", type=Path, help="Path to the log directory with model weights."
-    )
-    general_args.add_argument(
-        "-s", "--seed", default=default_seeds, dest="seed", nargs="+", type=int, help="Random seed at training and evaluation."
-    )
+    general_args.add_argument("-dir", "--data-dir", required=True, type=Path, help="Path to the parquet data directory.")
+    general_args.add_argument("-t", "--task-config", default="Mortality_At24Hours", help="Name of the task gin.")
+    general_args.add_argument("-m", "--model-config", default="LGBMClassifier", help="Name of the model gin.")
+    general_args.add_argument("-e", "--experiment-config", help="Name of the experiment gin.")
+    general_args.add_argument("-l", "--log-dir", type=Path, help="Path to the log directory with model weights.")
+    general_args.add_argument("-s", "--seed", default=SEEDS, nargs="+", type=int, help="Random seed at train and eval.")
 
-    #
     # MODEL TRAINING ARGUMENTS
-    #
     parser_prep_and_train = subparsers.add_parser("train", help="Preprocess data and train model.", parents=[parent_parser])
     train_args = parser_prep_and_train.add_argument_group("Train arguments")
-    train_args.add_argument(
-        "--reproducible", default=True, type=bool, dest="reproducible", help="Set torch to be reproducible."
-    )
-    train_args.add_argument("-rs", "--random-search", default=True, dest="rs", type=bool, help="Enable random search.")
-    train_args.add_argument("-o", "--overwrite", default=False, dest="overwrite", type=bool, help="Overwrite previous model.")
+    train_args.add_argument("-o", "--overwrite", default=False, action=BooleanOptionalAction, help="Overwrite previous model.")
+    train_args.add_argument("--reproducible", default=True, action=BooleanOptionalAction, help="Set torch to be reproducible.")
+    train_args.add_argument("-rs", "--random-search", default=True, action=BooleanOptionalAction, help="Enable random search.")
+    train_args.add_argument("-hp", "--hyperparams", nargs="+", help="Hyperparameters for model.")
 
-    hyper_args = parser_prep_and_train.add_argument_group("Hyperparameters")
-    for short, dest, tp, default, desc in cli_hyper_params:
-        hyper_args.add_argument(
-            f"-{short}", f"--{dest}".replace("_", "-"), default=default, dest=dest, type=tp, nargs="+", help=desc
-        )
-
-    #
     # EVALUATION PARSER
-    #
     evaluate_parser = subparsers.add_parser("evaluate", help="Evaluate trained model on data.", parents=[parent_parser])
-    evaluate_parser.add_argument("-c", "--train-config", required=True, dest="train_config", help="Original train gin.")
+    evaluate_parser.add_argument("-c", "--train-config", required=True, help="Original train gin.")
 
     return parser
 
 
+def to_correct_type(value):
+    try:
+        val = literal_eval(value)
+        return val if isinstance(val, list) else [val]
+    except ValueError:
+        return value
+
+
 def main(my_args=tuple(sys.argv[1:])):
     args = build_parser().parse_args(my_args)
-    hyper_params = [param[1] for param in cli_hyper_params]
+    hyperparams = {param.split('=')[0]: to_correct_type(param.split('=')[1]) for param in args.hyperparams}
 
     log_fmt = "%(asctime)s - %(levelname)s: %(message)s"
     logging.basicConfig(format=log_fmt)
@@ -105,7 +74,7 @@ def main(my_args=tuple(sys.argv[1:])):
         overwrite = False
         gin.parse_config_file(args.train_config)
         gin_config_files = [args.train_config]
-        gin_bindings, log_dir_bindings = get_bindings(hyper_params, args, log_dir_model)
+        gin_bindings, log_dir_bindings = get_bindings(hyperparams, log_dir_model)
     else:
         reproducible = args.reproducible
         overwrite = args.overwrite
@@ -120,12 +89,12 @@ def main(my_args=tuple(sys.argv[1:])):
             gin.parse_config_file(model_config)
             gin.parse_config_file(task_config)
             gin_config_files = [model_config, task_config]
-        gin_bindings, log_dir_bindings = get_bindings(hyper_params, args, log_dir_model)
-        if args.rs:
+        gin_bindings, log_dir_bindings = get_bindings(hyperparams, log_dir_model)
+        if args.random_search:
             reproducible = False
             attempt = 0
-            while Path.exists(log_dir_bindings) and attempt < max_rs_attempt:
-                gin_bindings, log_dir_bindings = get_bindings(hyper_params, args, log_dir_model, do_rs=True)
+            while Path.exists(log_dir_bindings) and attempt < MAX_ATTEMPTS:
+                gin_bindings, log_dir_bindings = get_bindings(hyperparams, log_dir_model, do_rs=True)
                 attempt += 1
             if Path.exists(log_dir_bindings):
                 raise Exception("Reached max attempt to find unexplored set of parameters parameters")
