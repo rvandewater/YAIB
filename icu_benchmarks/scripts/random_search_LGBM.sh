9c7b6d8de61f27199ffeba591e7a99d68fc284c4
Hendrik Schmidt <hendriknas@gmail.com>
2022-11-11 19:18:24 +0100

    Update random_search_LGBM.sh

--- icu_benchmarks/scripts/random_search_LGBM.sh
+++ icu_benchmarks/scripts/random_search_LGBM.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-for i in {1..5}
+for i in {1..50}
 do
-   echo "Welcome $i times"
+   python -m icu_benchmarks.run train \
+    -d ../data/mortality_seq/hirid \
+    -n hirid \
+    -t Mortality_At24Hours \
+    -m LGBMClassifier \
+    -c \
+    -s 1111 2222 3333 4444 5555
 done
