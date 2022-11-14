d4d5a9f67e7ad7a973c454f38bbe3412afafffd2
Hendrik Schmidt <hendriknas@gmail.com>
2022-11-14 18:07:19 +0100

    Create random_search_LSTM.sh

---
+++ icu_benchmarks/scripts/random_search_LSTM.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for i in {1..25}
+do
+   python -m icu_benchmarks.run train \
+    -d ../data/mortality_seq/hirid \
+    -n hirid \
+    -t Mortality_At24Hours \
+    -m LSTM \
+    -hp DLWrapper.train.patience=5
+    -c \
+    -s 1111 2222 3333
+done
