06c17fbcc4d82054d4a8cdc949c3b19774fd8659
Hendrik Schmidt <hendriknas@gmail.com>
2022-11-18 00:18:42 +0100

    Create calculate_average_scores.py

---
+++ icu_benchmarks/scripts/evaluate_results/calculate_average_scores.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import pickle
+import sys
+
+log_dir = Path(sys.argv[1])
+PR = 0
+AUC = 0
+num_seeds = 0
+
+for seed in log_dir.iterdir():
+    num_seeds += 1
+    with open(seed / "test_metrics.pkl", "rb") as f:
+        result = pickle.load(f)
+        PR += result['PR']
+        AUC += result['AUC']
+
+PR = PR / num_seeds
+AUC = AUC / num_seeds
+
+print(f"PR: {PR:.4f} | AUC: {AUC:.4f}")
