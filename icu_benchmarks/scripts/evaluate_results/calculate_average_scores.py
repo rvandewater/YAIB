e434754e019628445a0facb30d88f86cee4267cb
Hendrik Schmidt <hendriknas@gmail.com>
2022-11-22 13:31:19 +0100

    make evaluation easier

--- icu_benchmarks/scripts/evaluate_results/calculate_average_scores.py
+++ icu_benchmarks/scripts/evaluate_results/calculate_average_scores.py
@@ -3,19 +3,24 @@ import pickle
 import sys
 
 log_dir = Path(sys.argv[1])
-PR = 0
-AUC = 0
-num_seeds = 0
 
-for seed in log_dir.iterdir():
-    num_seeds += 1
-    with open(seed / "test_metrics.pkl", "rb") as f:
-        result = pickle.load(f)
-        PR += result['PR']
-        AUC += result['AUC']
+for dataset_dir in log_dir.iterdir():
+    for experiment_dir in dataset_dir.iterdir():
+        for source_dir in experiment_dir.iterdir():
+            PR = 0
+            AUC = 0
+            num_seeds = 0
+            run_dirs = sorted(list(source_dir.iterdir()))
+            for seed in run_dirs[-1].iterdir():
+                num_seeds += 1
+                with open(seed / "test_metrics.pkl", "rb") as f:
+                    result = pickle.load(f)
+                    PR += result['PR']
+                    AUC += result['AUC']
 
-PR = PR / num_seeds
-AUC = AUC / num_seeds
+            PR = PR / num_seeds
+            AUC = AUC / num_seeds
 
-print(f"PR: {PR:.4f}")
-print(f"AUC: {AUC:.4f}")
+            print(f"{experiment_dir.name} on {dataset_dir.name} {source_dir.name}")
+            print(f"PR: {PR:.4f}")
+            print(f"AUC: {AUC:.4f}")
--- icu_benchmarks/scripts/transfer/transfer.sh
+++ icu_benchmarks/scripts/transfer/transfer.sh
@@ -6,17 +6,14 @@ for var in ${names[@]}
 do
     for i in ${!names[@]}
     do
-        if [ $var != ${names[$i]} ];
-        then
-            icu-benchmarks evaluate \
-                -e $1 \
-                -l $2 \
-                -d $3$var \
-                -n $var \
-                -sn ${names[$i]} \
-                --source-dir $5${names[$i]}/${source_dirs[$i]} \
-                -c \
-                -s 1111 2222 3333 4444 5555
-        fi
+        icu-benchmarks evaluate \
+            -e $1 \
+            -l $2 \
+            -d $3$var \
+            -n $var \
+            -sn ${names[$i]} \
+            --source-dir $5${names[$i]}/${source_dirs[$i]} \
+            -c \
+            -s 1111 2222 3333 4444 5555
     done
 done
