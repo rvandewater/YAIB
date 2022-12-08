a4a716af3395df16844a42d27d95a1183d0ff0c1
Hendrik Schmidt <hendriknas@gmail.com>
2022-12-08 22:05:48 +0100

    refactor wrappers to pathlib

--- icu_benchmarks/models/wrappers.py
+++ icu_benchmarks/models/wrappers.py
@@ -1,6 +1,7 @@
 import inspect
 import logging
 import os
+from pathlib import Path
 import pickle
 
 import gin
@@ -64,7 +65,7 @@ class DLWrapper(object):
         self.optimizer = optimizer_fn(self.encoder.parameters())
         self.scaler = None
 
-    def set_log_dir(self, log_dir):
+    def set_log_dir(self, log_dir: Path):
         self.log_dir = log_dir
 
     def set_scaler(self, scaler):
@@ -168,7 +169,6 @@ class DLWrapper(object):
         batch_size=64,
         patience=10,
         min_delta=1e-4,
-        save_weights=True,
     ):
 
         self.set_metrics()
@@ -200,8 +200,8 @@ class DLWrapper(object):
 
         best_loss = float("inf")
         epoch_no_improvement = 0
-        train_writer = SummaryWriter(os.path.join(self.log_dir, "tensorboard", "train"))
-        val_writer = SummaryWriter(os.path.join(self.log_dir, "tensorboard", "val"))
+        train_writer = SummaryWriter(self.log_dir / "tensorboard" / "train")
+        val_writer = SummaryWriter(self.log_dir / "tensorboard" / "val")
 
         for epoch in range(epochs):
             # Train step
@@ -214,7 +214,7 @@ class DLWrapper(object):
             if val_loss <= best_loss - min_delta:
                 best_metrics = val_metric_results
                 epoch_no_improvement = 0
-                self.save_weights(epoch, os.path.join(self.log_dir, "model.torch"))
+                self.save_weights(epoch, self.log_dir / "model.torch")
                 best_loss = val_loss
                 logging.info("Validation loss improved to {:.4f} ".format(val_loss))
             else:
@@ -246,11 +246,11 @@ class DLWrapper(object):
             logging.info(train_string.format(*train_values))
             logging.info(val_string.format(*val_values))
 
-        with open(os.path.join(self.log_dir, "val_metrics.pkl"), "wb") as f:
+        with open(self.log_dir / "val_metrics.pkl", "wb") as f:
             best_metrics["loss"] = best_loss
             pickle.dump(best_metrics, f)
 
-        self.load_weights(os.path.join(self.log_dir, "model.torch"))  # We load back the best iteration
+        self.load_weights(self.log_dir / "model.torch")  # We load back the best iteration
 
     def test(self, dataset, weight):
         self.set_metrics()
@@ -259,7 +259,7 @@ class DLWrapper(object):
             weight = torch.FloatTensor(weight).to(self.device)
         test_loss, test_metrics = self.evaluate(test_loader, self.metrics, weight)
 
-        with open(os.path.join(self.log_dir, "test_metrics.pkl"), "wb") as f:
+        with open(self.log_dir / "test_metrics.pkl", "wb") as f:
             test_metrics["loss"] = test_loss
             pickle.dump(test_metrics, f)
         for key, value in test_metrics.items():
@@ -299,7 +299,7 @@ class MLWrapper(object):
         self.model = model
         self.scaler = None
 
-    def set_log_dir(self, log_dir):
+    def set_log_dir(self, log_dir: Path):
         self.log_dir = log_dir
 
     def set_metrics(self, labels):
@@ -384,11 +384,11 @@ class MLWrapper(object):
 
         if save_weights:
             if model_type == "lgbm":
-                self.save_weights(save_path=os.path.join(self.log_dir, "model.txt"), model_type=model_type)
+                self.save_weights(save_path=(self.log_dir / "model.txt"), model_type=model_type)
             else:
-                self.save_weights(save_path=os.path.join(self.log_dir, "model.joblib"), model_type=model_type)
+                self.save_weights(save_path=(self.log_dir / "model.joblib"), model_type=model_type)
 
-        with open(os.path.join(self.log_dir, "val_metrics.pkl"), "wb") as f:
+        with open(self.log_dir / "val_metrics.pkl", "wb") as f:
             pickle.dump(val_metric_results, f)
 
     def test(self, dataset, weight):
@@ -411,7 +411,7 @@ class MLWrapper(object):
             test_values.append(test_metric_results[name])
 
         logging.info(test_string.format(*test_values))
-        with open(os.path.join(self.log_dir, "test_metrics.pkl"), "wb") as f:
+        with open(self.log_dir / "test_metrics.pkl", "wb") as f:
             pickle.dump(test_metric_results, f)
 
         return test_loss
