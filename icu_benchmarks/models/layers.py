7bd030c1c1bbaa47dd85d54f647b7742cbc08ba3
rvandewater <r.p.vandewater@gmail.com>
2022-12-13 14:48:11 +0100

    Unused import

--- icu_benchmarks/models/layers.py
+++ icu_benchmarks/models/layers.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils import weight_norm
-import icu_benchmarks.models.wrappers as wrapper
 
 
 @gin.configurable("masking")
