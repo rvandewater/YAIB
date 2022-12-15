335196f5ef96d4b88189525a631f7a128fd61c11
rvandewater <r.p.vandewater@gmail.com>
2022-12-15 17:52:46 +0100

    Support for multiple folds in one figure

--- icu_benchmarks/scripts/plotting/plotting.py
+++ icu_benchmarks/scripts/plotting/plotting.py
@@ -17,13 +17,16 @@ def plot_results(log_dir, metrics_path):
 
 
 def plot_fold(metrics, save_dir):
-    plot_calibration_curve(metrics["Calibration"][0], metrics["Calibration"][1], save_dir)
-    plot_prc(metrics["PRC"][0], metrics["PRC"][1], save_dir)
-    plot_roc(metrics["ROC"][0], metrics["ROC"][1], metrics["AUC"], save_dir)
+    plot_calibration_curve({"fold": metrics}, save_dir)
+    plot_prc({"fold": metrics}, save_dir)
+    plot_roc({"fold": metrics}, save_dir)
 
 
-def plot_roc(fpr, tpr, auc, save_dir):
-    plt.plot(fpr, tpr, label="ROC curve (area = %0.3f)" % auc)
+def plot_roc(results, save_dir):
+    for fold in results:
+        fold_result = results[fold]
+        auc = fold_result["AUC"]
+        plt.plot(fold_result["ROC"][0], fold_result["ROC"][1], label=f"ROC curve {fold} {auc:0.3f}")
     plt.plot([0, 1], [0, 1], "k--")  # random predictions curve
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.0])
@@ -35,8 +38,11 @@ def plot_roc(fpr, tpr, auc, save_dir):
     plt.clf()
 
 
-def plot_prc(rec, prec, auprc, save_dir):
-    plt.plot(rec, prec, label="PRC curve (area = %0.3f)")
+def plot_prc(results, save_dir):
+    for fold in results:
+        fold_result = results[fold]
+        prc = fold_result["PR"]
+        plt.plot(fold_result["PRC"][0], fold_result["PRC"][1], label=f"PRC curve {fold} {prc:0.3f}")
     plt.xlim([0.0, 1.0])
     plt.ylim([0.5, 1.0])
     plt.xlabel("Recall")
@@ -47,8 +53,10 @@ def plot_prc(rec, prec, auprc, save_dir):
     plt.clf()
 
 
-def plot_calibration_curve(mpp, fop, save_dir):
-    plt.plot(mpp, fop, label="Calibration Curve")
+def plot_calibration_curve(results, save_dir):
+    for fold in results:
+        fold_result = results[fold]
+        plt.plot(fold_result["Calibration"][0], fold_result["Calibration"][1], label=f"Calibration curve {fold}")
     plt.plot([0, 1], [0, 1], "k--", label="Perfect Calibration")  # random predictions curve
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.0])
