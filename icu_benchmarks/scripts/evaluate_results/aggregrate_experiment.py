17612bdcd86432c84848b9a6b7bee0351f7a570d
rvandewater <r.p.vandewater@gmail.com>
2023-01-03 16:58:49 +0100

    Rounding

--- icu_benchmarks/scripts/evaluate_results/aggregrate_experiment.py
+++ icu_benchmarks/scripts/evaluate_results/aggregrate_experiment.py
@@ -29,10 +29,23 @@ for dataset in log_parent.iterdir():
                     std = "NaN"
                     ci_95 = "NaN"
                 aggregated.loc[len(aggregated.index)] = [dataset.name, model.name, avg, std, ci_95]
+
                 # print(aggregated[dataset.name, model.name])
                 # aggregated = pd.concat([aggregated, pd.DataFrame({"Dataset": dataset.name, "Model": model.name})], ignore_index=True)
                     # = json.load(open(log_time / "accumulated_test_metrics.json"))
-# aggregated = aggregated.sort_values(by=["Model"])
+nan_rows = aggregated[aggregated["95% CI"]=="NaN"]
+aggregated = aggregated[aggregated["95% CI"]!="NaN"]
+
+aggregated["Avg"] = pd.to_numeric(aggregated["Avg"], errors="coerce")
+aggregated["Std"] = pd.to_numeric(aggregated["Std"], errors="coerce")
+aggregated["95% CI"] = aggregated["95% CI"].apply(lambda x: (pd.to_numeric(x[1])*100, pd.to_numeric(x[0])*100))
+aggregated[["Avg", "Std"]] = aggregated[["Avg", "Std"]].apply(lambda x: x*100)
+# aggregated[""] = aggregated.apply(pd.to_numeric, columns= ["Avg", "Std"], errors='coerce')
 aggregated = aggregated.round(2)
-pd.to_numeric(aggregated["Avg"])
-print(aggregated.round(2).to_markdown())
+aggregated["95% CI"] = aggregated["95% CI"].apply(lambda x: tuple(map(lambda y: round(y,2),x)))
+aggregated = aggregated.append(nan_rows)
+aggregated = aggregated.set_index(["Dataset"])
+# aggregated = aggregated.reset_index(drop=True)
+# pd.to_numeric(aggregated["Avg"])
+aggregated = aggregated.sort_values(by=["Dataset", "Model"], ascending=[True, True])
+print(aggregated.to_markdown())
