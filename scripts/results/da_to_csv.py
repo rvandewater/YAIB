88a79e0900ffd42f96ad65e6637e0bd5961ef09b
Hendrik Schmidt <hendriknas@gmail.com>
2023-01-13 15:12:26 +0100

    Update da_to_csv.py

--- scripts/results/da_to_csv.py
+++ scripts/results/da_to_csv.py
@@ -8,11 +8,12 @@ for metric in ["AUC", "PR"]:
         if endpoint.is_dir():
             with open(models_dir / f'{endpoint.name}_{metric}_results.csv', 'w') as csv_file:
                 writer = csv.writer(csv_file)
+                info = ["model", "target", "target_size"]
                 source_names = ['target', 'aumc', 'eicu', 'hirid', 'miiv', 'convex_combination_without_target', 'convex_combination_0.1', 'convex_combination_0.2', 'convex_combination_0.5', 'convex_combination_1', 'convex_combination_2', 'convex_combination_5']
                 stats = ['avg', 'std', 'CI_0.95']
                 # combine fieldnames and stats
                 full_fields = [f'{source}_{stat}' for source in source_names for stat in stats]
-                writer = csv.DictWriter(csv_file, fieldnames=full_fields)
+                writer = csv.DictWriter(csv_file, fieldnames=info+full_fields)
 
                 writer.writeheader()
                 for model in endpoint.iterdir():
@@ -26,4 +27,7 @@ for metric in ["AUC", "PR"]:
                                 # source_metrics = [[metr[0]["avg"], metr[0]["std"], metr[0]["CI_0.95"]] for metr in source_metrics]
                                 # source_metrics_flat = [item for sublist in source_metrics for item in sublist]
                                 # writer.writerow([model.name, target.name, target_size.name] + source_metrics_flat)
-                                writer.writerow({f'{source}_{stat}': source_metrics[metric][0][stat] for source, source_metrics in results.items() for stat in stats})
+
+                                info = [model.name, target.name, target_size.name]
+                                metrics_row = {f'{source}_{stat}': source_metrics[metric][0][stat] for source, source_metrics in results.items() for stat in stats}
+                                writer.writerow(info + metrics_row)
