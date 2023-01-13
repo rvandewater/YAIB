22b7f61930df1a132e1dc9baf35cad239b111450
Hendrik Schmidt <hendriknas@gmail.com>
2023-01-13 15:08:44 +0100

    Update da_to_csv.py

--- scripts/results/da_to_csv.py
+++ scripts/results/da_to_csv.py
@@ -8,6 +8,13 @@ for metric in ["AUC", "PR"]:
         if endpoint.is_dir():
             with open(models_dir / f'{endpoint.name}_{metric}_results.csv', 'w') as csv_file:
                 writer = csv.writer(csv_file)
+                source_names = ['target', 'aumc', 'eicu', 'hirid', 'miiv', 'convex_combination_without_target', 'convex_combination_0.1', 'convex_combination_0.2', 'convex_combination_0.5', 'convex_combination_1', 'convex_combination_2', 'convex_combination_5']
+                stats = ['avg', 'std', 'CI_0.95']
+                # combine fieldnames and stats
+                full_fields = [f'{source}_{stat}' for source in source_names for stat in stats]
+                writer = csv.DictWriter(csv_file, fieldnames=full_fields)
+
+                writer.writeheader()
                 for model in endpoint.iterdir():
                     if model.name == "LSTM":
                         continue
@@ -15,7 +22,8 @@ for metric in ["AUC", "PR"]:
                         for target_size in target.iterdir():
                             with open(target_size / 'averaged_source_metrics.json', 'r') as f:
                                 results = json.load(f)
-                                source_metrics = [source_metrics[metric] for source_name, source_metrics in results.items()]
-                                source_metrics = [[metr[0]["avg"], metr[0]["std"], metr[0]["CI_0.95"]] for metr in source_metrics]
-                                source_metrics_flat = [item for sublist in source_metrics for item in sublist]
-                                writer.writerow([model.name, target.name, target_size.name] + source_metrics_flat)
+                                # source_metrics = [source_metrics[metric] for source_name, source_metrics in results.items()]
+                                # source_metrics = [[metr[0]["avg"], metr[0]["std"], metr[0]["CI_0.95"]] for metr in source_metrics]
+                                # source_metrics_flat = [item for sublist in source_metrics for item in sublist]
+                                # writer.writerow([model.name, target.name, target_size.name] + source_metrics_flat)
+                                writer.writerow({f'{source}_{stat}': source_metrics[metric][stat] for source, source_metrics in results.items() for stat in stats})
