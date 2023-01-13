11d80e2e7c123f39ffd65e1877ffe2e5c3261f44
Hendrik Schmidt <hendriknas@gmail.com>
2023-01-13 14:28:01 +0100

    Update da_to_csv.py

--- scripts/results/da_to_csv.py
+++ scripts/results/da_to_csv.py
@@ -5,13 +5,14 @@ import csv
 models_dir = Path("../DA_logs")
 for metric in ["AUC", "PR"]:
     for endpoint in models_dir.iterdir():
-        with open(models_dir / f'{endpoint.name}_{metric}_results.csv', 'w') as csv_file:
-            writer = csv.writer(csv_file)
-            for model in endpoint.iterdir():
-                if model.name == "LSTM":
-                    continue
-                for target in model.iterdir():
-                    for target_size in target.iterdir():
-                        with open(target_size / 'averaged_source_metrics.json', 'r') as f:
-                            results = json.load(f)
-                            writer.writerow([model.name, target, target_size] + [source_metrics[metric] for source_name, source_metrics in results.items()])
+        if endpoint.is_dir():
+            with open(models_dir / f'{endpoint.name}_{metric}_results.csv', 'w') as csv_file:
+                writer = csv.writer(csv_file)
+                for model in endpoint.iterdir():
+                    if model.name == "LSTM":
+                        continue
+                    for target in model.iterdir():
+                        for target_size in target.iterdir():
+                            with open(target_size / 'averaged_source_metrics.json', 'r') as f:
+                                results = json.load(f)
+                                writer.writerow([model.name, target, target_size] + [source_metrics[metric] for source_name, source_metrics in results.items()])
