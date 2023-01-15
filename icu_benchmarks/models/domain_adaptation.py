0c97842330b913f9ea1b66a03439682f1c1399bf
Hendrik Schmidt <hendriknas@gmail.com>
2023-01-15 02:56:29 +0100

    Update domain_adaptation.py

--- icu_benchmarks/models/domain_adaptation.py
+++ icu_benchmarks/models/domain_adaptation.py
@@ -254,8 +254,8 @@ def domain_adaptation(
                 for baseline, predictions in test_predictions.items():
                     # logging.info("Evaluating model: {}".format(baseline))
                     fold_results[baseline] = calculate_metrics(predictions, test_labels)
-                    print(predictions.min())
-                    print(predictions.max())
+                    print(min(predictions))
+                    print(max(predictions))
                 # evaluate baselines
 
                 # evaluate convex combination of models
