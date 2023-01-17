db0393f711406bda96e80ab7efdf0eaf6fd706dd
Hendrik Schmidt <hendriknas@gmail.com>
2023-01-17 16:37:13 +0100

    Update da_to_csv.py

--- scripts/results/da_to_csv.py
+++ scripts/results/da_to_csv.py
@@ -36,15 +36,16 @@ for metric in ["AUC", "PR"]:
                     for target in ["aumc", "eicu", "hirid", "miiv"]:
                         target_sizes = ["target_500", "target_1000", "target_2000"]
                         for target_size in target_sizes:
-                            with open(model / target / target_size / "averaged_source_metrics.json", "r") as f:
-                                results = json.load(f)
+                            if (model / target / target_size).exists():
+                                with open(model / target / target_size / "averaged_source_metrics.json", "r") as f:
+                                    results = json.load(f)
 
-                                row_data = {"model": model.name, "target": target, "target_size": target_size}
-                                for stat in stats_basis:
-                                    for source, source_metrics in results.items():
-                                        if stat == "CI_0.95":
-                                            row_data[f"{source}_{stat}_min"] = source_metrics[metric][0][stat][0] * 100
-                                            row_data[f"{source}_{stat}_max"] = source_metrics[metric][0][stat][1] * 100
-                                        else:
-                                            row_data[f"{source}_{stat}"] = source_metrics[metric][0][stat] * 100
-                                writer.writerow(row_data)
+                                    row_data = {"model": model.name, "target": target, "target_size": target_size}
+                                    for stat in stats_basis:
+                                        for source, source_metrics in results.items():
+                                            if stat == "CI_0.95":
+                                                row_data[f"{source}_{stat}_min"] = source_metrics[metric][0][stat][0] * 100
+                                                row_data[f"{source}_{stat}_max"] = source_metrics[metric][0][stat][1] * 100
+                                            else:
+                                                row_data[f"{source}_{stat}"] = source_metrics[metric][0][stat] * 100
+                                    writer.writerow(row_data)
