1adcd0976e0d6268f64738c3d9e171fcc8dcd60c
rvandewater <r.p.vandewater@gmail.com>
2023-01-17 12:40:41 +0100

    Brought Custompreprocessor in new format

--- tests/resources/custom_preprocessor.py
+++ tests/resources/custom_preprocessor.py
@@ -12,33 +12,35 @@ from icu_benchmarks.data.preprocessor import Preprocessor
 class CustomPreprocessor(Preprocessor):
     def __init__(
         self,
-        data,
-        vars: dict[str],
         generate_features: bool = True,
         scaling: bool = True,
     ):
         """
         Args:
-            data: Train, validation and test data dictionary. Further divided in static, dynamic, and outcome.
-            vars: Variables for static, dynamic, outcome.
             generate_features: Generate features for static data.
             scaling: Scaling of dynamic and static data.
         Returns:
             Preprocessed data.
         """
-        self.data = data
-        self.vars = vars
         self.generate_features = generate_features
         self.scaling = scaling
 
-    def apply(self):
+    # TODO: pass data and vars as arguments
+    def apply(self, data, vars):
+        """
+        Args:
+            data: Train, validation and test data dictionary. Further divided in static, dynamic, and outcome.
+            vars: Variables for static, dynamic, outcome.
+        Returns:
+            Preprocessed data.
+        """
         logging.info("Preprocessor static features.")
-        self.data = self.process_static()
-        self.data = self.process_dynamic()
-        return self.data
+        data = self.process_static(data, vars)
+        data = self.process_dynamic(data, vars)
+        return data
 
-    def process_static(self):
-        sta_rec = Recipe(self.data["train"]["STATIC"], [], self.vars["STATIC"])
+    def process_static(self, data, vars):
+        sta_rec = Recipe(data["train"]["STATIC"], [], vars["STATIC"])
         if self.scaling:
             sta_rec.add_step(StepScale())
 
@@ -46,30 +48,31 @@ class CustomPreprocessor(Preprocessor):
         sta_rec.add_step(StepSklearn(SimpleImputer(missing_values=None, strategy="most_frequent"), sel=has_type("object")))
         sta_rec.add_step(StepSklearn(LabelEncoder(), sel=has_type("object"), columnwise=True))
 
-        data = self.apply_recipe_to_splits(sta_rec, self.data, "STATIC")
+        data = self.apply_recipe_to_splits(sta_rec, data, "STATIC")
 
         return data
 
-    def process_dynamic(self):
-        vars = self.vars
-        dyn_rec = Recipe(self.data["train"]["DYNAMIC"], [], vars["DYNAMIC"], vars["GROUP"], vars["SEQUENCE"])
+    def process_dynamic(self, data, vars):
+        dyn_rec = Recipe(data["train"]["DYNAMIC"], [], vars["DYNAMIC"], vars["GROUP"], vars["SEQUENCE"])
         if self.scaling:
             dyn_rec.add_step(StepScale())
         dyn_rec.add_step(StepSklearn(MissingIndicator(), sel=all_of(vars["DYNAMIC"]), in_place=False))
         dyn_rec.add_step(StepImputeFill(method="ffill"))
         dyn_rec.add_step(StepImputeFill(value=0))
         if self.generate_features:
-            dyn_rec = self.dynamic_feature_generation(dyn_rec)
-
-        data = self.apply_recipe_to_splits(dyn_rec, self.data, "DYNAMIC")
+            dyn_rec = self.dynamic_feature_generation(dyn_rec, all_of(vars["DYNAMIC"]))
+        data = self.apply_recipe_to_splits(dyn_rec, data, "DYNAMIC")
         return data
 
-    def dynamic_feature_generation(self, data):
-        data.add_step(StepHistorical(sel=all_of(self.vars["DYNAMIC"]), fun=Accumulator.MIN, suffix="min_hist"))
-        data.add_step(StepHistorical(sel=all_of(self.vars["DYNAMIC"]), fun=Accumulator.MAX, suffix="max_hist"))
-        data.add_step(StepHistorical(sel=all_of(self.vars["DYNAMIC"]), fun=Accumulator.COUNT, suffix="count_hist"))
-        data.add_step(StepHistorical(sel=all_of(self.vars["DYNAMIC"]), fun=Accumulator.MEAN, suffix="mean_hist"))
+    def dynamic_feature_generation(self, data, dynamic_vars):
+        data.add_step(StepHistorical(sel=dynamic_vars, fun=Accumulator.MIN, suffix="min_hist"))
+        data.add_step(StepHistorical(sel=dynamic_vars, fun=Accumulator.MAX, suffix="max_hist"))
+        data.add_step(StepHistorical(sel=dynamic_vars, fun=Accumulator.COUNT, suffix="count_hist"))
+        data.add_step(StepHistorical(sel=dynamic_vars, fun=Accumulator.MEAN, suffix="mean_hist"))
         return data
+
+    def to_cache_string(self):
+        return super().to_cache_string() + f"_{self.generate_features}_{self.scaling}"
 
     @staticmethod
     def apply_recipe_to_splits(recipe: Recipe, data: dict[dict[pd.DataFrame]], type: str) -> dict[dict[pd.DataFrame]]:
