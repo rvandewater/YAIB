4018589b8d19804907d1fe4aefd69cde997fea69
rvandewater <r.p.vandewater@gmail.com>
2023-01-27 10:53:33 +0100

    Cleaning

--- scripts/evaluate_results/aggregrate_experiment.py
+++ scripts/evaluate_results/aggregrate_experiment.py
@@ -1,11 +1,6 @@
 import json
-from argparse import ArgumentParser, BooleanOptionalAction
-from datetime import datetime
-import logging
-import gin
 import math
 from pathlib import Path
-
 import pandas as pd
 
 
@@ -16,17 +11,32 @@ def aggregate_results(
     include_unfinished=False,
     iterations=5,
     decimals=2,
+    scale=100,
     sort=["Dataset", "Model"],
     datasets=["miiv", "aumc", "hirid", "eicu"],
+    results_file="accumulated_test_metrics.json"
 ):
+    """
+    Aggregate results from a log directory.
+    Args:
+        log_dir: Log directory stub.
+        models: List of models to include in the results.
+        metric_type: Metric to aggregate.
+        include_unfinished: Include experiments that have not finished.
+        iterations: Correct standard deviation for amount of iterations.
+        decimals: Calculate results to x decimals.
+        sort: Where to sort on.
+        datasets: Which datasets to include.
+        results_file: Name of the results file.
+    """
     results = pd.DataFrame(columns=["Time", "Dataset", "Model", "Average", "Std", "95% CI", "Execution Time"])
     for dataset in log_dir.iterdir():
         if dataset.is_dir() and dataset.name in datasets:
             for task in dataset.iterdir():
                 for model in task.iterdir():
                     for log_time in model.iterdir():
-                        if Path(log_time / "accumulated_test_metrics.json").exists():
-                            test_metrics = json.load(open(log_time / "accumulated_test_metrics.json"))
+                        if Path(log_time / results_file).exists():
+                            test_metrics = json.load(open(log_time / results_file))
                             avg = test_metrics["avg"][metric_type]
                             std = test_metrics["std"][metric_type]
                             ci_95 = test_metrics["CI_0.95"][metric_type]
@@ -42,14 +52,19 @@ def aggregate_results(
                             time = "NaN"
                             results.loc[len(results.index)] = [log_time.name, dataset.name, model.name, avg, std, ci_95, time]
 
+    # Exclude nan rows for calculations
     nan_rows = results[results["95% CI"] == "NaN"]
     results = results[results["95% CI"] != "NaN"]
-    #results["Time"] = results["Time"].apply(lambda x: pd.to_datetime(x, format="%H:%M:%S"))
+
+    # Convert to units
     results["Execution Time"] = pd.to_timedelta(results["Execution Time"])
     results["Average"] = pd.to_numeric(results["Average"], errors="coerce")
     results["Std"] = pd.to_numeric(results["Std"], errors="coerce")
-    results["95% CI"] = results["95% CI"].apply(lambda x: (pd.to_numeric(x[1]) * 100, pd.to_numeric(x[0]) * 100))
-    results[["Average", "Std"]] = results[["Average", "Std"]].apply(lambda x: x * 100)
+
+    # Scaling
+    results["95% CI"] = results["95% CI"].apply(lambda x: (pd.to_numeric(x[1]) * scale, pd.to_numeric(x[0]) * scale))
+    results[["Average", "Std"]] = results[["Average", "Std"]].apply(lambda x: x * scale)
+
     # Correct std for amount of iterations
     if iterations > 1:
         results["Std"] = results["Std"].apply(lambda x: x / math.sqrt(iterations))
@@ -59,9 +74,13 @@ def aggregate_results(
     results["95% CI"] = results["95% CI"].apply(lambda x: tuple(map(lambda y: round(y, decimals), x)))
 
     # Append unfinished results
-    results = results.append(nan_rows)
+    results = pd.concat([results, nan_rows])
+
+    # Sort
     results = results.set_index(["Dataset"])
     results = results.sort_values(by=sort, ascending=[True, True])
+
+    # Exclude datasets
     results = results[results["Model"].isin(models)]
     print(results.to_markdown())
 
