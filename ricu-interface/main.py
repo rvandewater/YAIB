23f49a6b0deb10d3ec121ef96dcf3f91dcca1a5b
rvandewater <r.p.vandewater@gmail.com>
2023-05-16 14:01:08 +0200

    out of memory workaround and experimentation

--- ricu-interface/main.py
+++ ricu-interface/main.py
@@ -1,16 +1,38 @@
+import pathlib
+
+# import rpy2
+# import rpy2.robjects as robjects
+# from rpy2.robjects.packages import importr
+# print(rpy2.__version__)
+
+r_files = ['.RData', '.RHistory', '.RProfile']
+for rf in r_files:
+    p = pathlib.Path.cwd() / rf
+    try:
+        p.rename(p.with_suffix('.ignore'))
+    except FileNotFoundError:
+        pass
 import rpy2
-import rpy2.robjects as robjects
+from rpy2 import robjects
 from rpy2.robjects.packages import importr
-print(rpy2.__version__)
+for rf in r_files:
+    p = pathlib.Path.cwd() / rf
+    p = p.with_suffix('.ignore')
+    try:
+        p.rename(p.with_suffix(''))
+    except FileNotFoundError:
+        pass
+
 
 base = importr('base')
 utils = importr('utils')
-utils.chooseCRANmirror(ind=1)
-utils.install_packages('renv')
 renv = importr('renv')
-
-renv.record("renv@0.17.3")
+utils.chooseCRANmirror(ind=1)
+renv.install('rtools')
+# utils.install_packages('rtools')
+# # utils.install_packages('renv')
+#
 renv.restore()
-ricu = importr('ricu')
+# ricu = importr('ricu')
 
 # ricu.download_src("miiv", data_dir = "C:\\Users\\Robin van de Water\\Documents\\Datasets")
