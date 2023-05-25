51545f8c475a32e026c19ebf1126ec939fdc4d35
rvandewater <r.p.vandewater@gmail.com>
2023-05-25 13:22:49 +0200

    Refactored preprocessing and fixed bugs

--- icu_benchmarks/data/preprocessor.py
+++ icu_benchmarks/data/preprocessor.py
@@ -24,12 +24,15 @@ class Preprocessor:
     def to_cache_string(self):
         return f"{self.__class__.__name__}"
 
+    def set_imputation_model(self, imputation_model):
+        self.imputation_model = imputation_model
+        if self.imputation_model is not None:
+            update_wandb_config({"imputation_model": self.imputation_model.__class__.__name__})
+
 
 @gin.configurable("base_classification_preprocessor")
 class DefaultClassificationPreprocessor(Preprocessor):
-    def __init__(
-            self, generate_features: bool = True, scaling: bool = True, use_static_features: bool = True
-    ):
+    def __init__(self, generate_features: bool = True, scaling: bool = True, use_static_features: bool = True):
         """
         Args:
             generate_features: Generate features for dynamic data.
@@ -43,12 +46,7 @@ class DefaultClassificationPreprocessor(Preprocessor):
         self.use_static_features = use_static_features
         self.imputation_model = None
 
-    def set_imputation_model(self, imputation_model):
-        self.imputation_model = imputation_model
-        if self.imputation_model is not None:
-            update_wandb_config({"imputation_model": self.imputation_model.__class__.__name__})
-
-    def apply(self, data, vars):
+    def apply(self, data, vars) -> dict[dict[pd.DataFrame]]:
         """
         Args:
             data: Train, validation and test data dictionary. Further divided in static, dynamic, and outcome.
@@ -57,10 +55,10 @@ class DefaultClassificationPreprocessor(Preprocessor):
             Preprocessed data.
         """
         logging.info("Preprocessing dynamic features.")
-        data = self.process_dynamic(data, vars)
+        data = self._process_dynamic(data, vars)
         if self.use_static_features:
             logging.info("Preprocessing static features.")
-            data = self.process_static(data, vars)
+            data = self._process_static(data, vars)
 
             # Set index to grouping variable
             data[Split.train][Segment.static] = data[Split.train][Segment.static].set_index(vars["GROUP"])
@@ -89,7 +87,7 @@ class DefaultClassificationPreprocessor(Preprocessor):
         data[Split.test][Segment.features] = data[Split.test].pop(Segment.dynamic)
         return data
 
-    def process_static(self, data, vars):
+    def _process_static(self, data, vars):
         sta_rec = Recipe(data[Split.train][Segment.static], [], vars[Segment.static])
         if self.scaling:
             sta_rec.add_step(StepScale())
@@ -98,18 +96,18 @@ class DefaultClassificationPreprocessor(Preprocessor):
         sta_rec.add_step(StepSklearn(SimpleImputer(missing_values=None, strategy="most_frequent"), sel=has_type("object")))
         sta_rec.add_step(StepSklearn(LabelEncoder(), sel=has_type("object"), columnwise=True))
 
-        data = self.apply_recipe_to_splits(sta_rec, data, Segment.static)
+        data = apply_recipe_to_splits(sta_rec, data, Segment.static)
 
         return data
 
-    def model_impute(self, data, group=None):
+    def _model_impute(self, data, group=None):
         dataset = ImputationPredictionDataset(data, group, self.imputation_model.trained_columns)
         input_data = torch.cat([data_point.unsqueeze(0) for data_point in dataset], dim=0)
         self.imputation_model.eval()
         with torch.no_grad():
-            logging.info("predicting...")
+            logging.info(f"Imputing with {self.imputation_model.__class__.__name__}.")
             imputation = self.imputation_model.predict(input_data)
-            logging.info("done predicting")
+            logging.info("Imputation done.")
         assert imputation.isnan().sum() == 0
         data = data.copy()
         data.loc[:, self.imputation_model.trained_columns] = imputation.flatten(end_dim=1).to("cpu")
@@ -117,7 +115,7 @@ class DefaultClassificationPreprocessor(Preprocessor):
             data.drop(columns=group, inplace=True)
         return data
 
-    def process_dynamic(self, data, vars):
+    def _process_dynamic(self, data, vars):
         dyn_rec = Recipe(data[Split.train][Segment.dynamic], [], vars[Segment.dynamic], vars["GROUP"], vars["SEQUENCE"])
         if self.scaling:
             dyn_rec.add_step(StepScale())
@@ -127,8 +125,8 @@ class DefaultClassificationPreprocessor(Preprocessor):
         dyn_rec.add_step(StepImputeFill(method="ffill"))
         dyn_rec.add_step(StepImputeFill(value=0))
         if self.generate_features:
-            dyn_rec = self.dynamic_feature_generation(dyn_rec, all_of(vars[Segment.dynamic]))
-        data = self.apply_recipe_to_splits(dyn_rec, data, Segment.dynamic)
+            dyn_rec = self._dynamic_feature_generation(dyn_rec, all_of(vars[Segment.dynamic]))
+        data = apply_recipe_to_splits(dyn_rec, data, Segment.dynamic)
         # data[Split.train][Segment.dynamic] = data[Split.train][Segment.dynamic].loc[
         #     :, vars[Segment.dynamic] + [vars["GROUP"], vars["SEQUENCE"]]
         # ]
@@ -140,7 +138,7 @@ class DefaultClassificationPreprocessor(Preprocessor):
         # ]
         return data
 
-    def dynamic_feature_generation(self, data, dynamic_vars):
+    def _dynamic_feature_generation(self, data, dynamic_vars):
         logging.debug("Adding dynamic feature generation.")
         data.add_step(StepHistorical(sel=dynamic_vars, fun=Accumulator.MIN, suffix="min_hist"))
         data.add_step(StepHistorical(sel=dynamic_vars, fun=Accumulator.MAX, suffix="max_hist"))
@@ -154,29 +152,12 @@ class DefaultClassificationPreprocessor(Preprocessor):
                 + f"_classification_{self.generate_features}_{self.scaling}_{self.imputation_model.__class__.__name__}"
         )
 
-    @staticmethod
-    def apply_recipe_to_splits(recipe: Recipe, data: dict[dict[pd.DataFrame]], type: str) -> dict[dict[pd.DataFrame]]:
-        """Fits and transforms the training features, then transforms the validation and test features with the recipe.
-
-        Args:
-            recipe: Object containing info about the features and steps.
-            data: Dict containing 'train', 'val', and 'test' and types of features per Split.
-            type: Whether to apply recipe to dynamic features, static features or outcomes.
-
-        Returns:
-            Transformed features divided into 'train', 'val', and 'test'.
-        """
-        data[Split.train][type] = recipe.prep()
-        data[Split.val][type] = recipe.bake(data[Split.val][type])
-        data[Split.test][type] = recipe.bake(data[Split.test][type])
-        return data
-
 
 @gin.configurable("base_regression_preprocessor")
 class DefaultRegressionPreprocessor(DefaultClassificationPreprocessor):
     # Override base classification preprocessor
     def __init__(
-            self, generate_features: bool = True, scaling: bool = True, use_static_features: bool = True, vars: dict = None,
+            self, generate_features: bool = True, scaling: bool = True, use_static_features: bool = True,
             outcome_max=None, outcome_min=None
     ):
         """
@@ -189,7 +170,7 @@ class DefaultRegressionPreprocessor(DefaultClassificationPreprocessor):
         Returns:
             Preprocessed data.
         """
-        super().__init__(generate_features, scaling, use_static_features, vars)
+        super().__init__(generate_features, scaling, use_static_features)
         self.outcome_max = outcome_max
         self.outcome_min = outcome_min
 
@@ -201,45 +182,13 @@ class DefaultRegressionPreprocessor(DefaultClassificationPreprocessor):
         Returns:
             Preprocessed data.
         """
-        logging.info("Preprocessing dynamic features.")
-
-        data = self.process_dynamic(data, vars)
-
         for split in [Split.train, Split.val, Split.test]:
-            data = self.process_outcome(data, vars, split)
+            data = self._process_outcome(data, vars, split)
 
-        if self.use_static_features:
-            logging.info("Preprocessing static features.")
-            data = self.process_static(data, vars)
-
-            # Set index to grouping variable
-            data[Split.train][Segment.static] = data[Split.train][Segment.static].set_index(vars["GROUP"])
-            data[Split.val][Segment.static] = data[Split.val][Segment.static].set_index(vars["GROUP"])
-            data[Split.test][Segment.static] = data[Split.test][Segment.static].set_index(vars["GROUP"])
-
-            # Join static and dynamic data.
-            data[Split.train][Segment.dynamic] = data[Split.train][Segment.dynamic].join(
-                data[Split.train][Segment.static], on=vars["GROUP"]
-            )
-            data[Split.val][Segment.dynamic] = data[Split.val][Segment.dynamic].join(
-                data[Split.val][Segment.static], on=vars["GROUP"]
-            )
-            data[Split.test][Segment.dynamic] = data[Split.test][Segment.dynamic].join(
-                data[Split.test][Segment.static], on=vars["GROUP"]
-            )
-
-            # Remove static features from splits
-            data[Split.train][Segment.features] = data[Split.train].pop(Segment.static)
-            data[Split.val][Segment.features] = data[Split.val].pop(Segment.static)
-            data[Split.test][Segment.features] = data[Split.test].pop(Segment.static)
-
-        # Create feature splits
-        data[Split.train][Segment.features] = data[Split.train].pop(Segment.dynamic)
-        data[Split.val][Segment.features] = data[Split.val].pop(Segment.dynamic)
-        data[Split.test][Segment.features] = data[Split.test].pop(Segment.dynamic)
+        data = super().apply(data, vars)
         return data
 
-    def process_outcome(self, data, vars, split):
+    def _process_outcome(self, data, vars, split):
         logging.debug(f"Processing {split} outcome values.")
         outcome_rec = Recipe(data[split][Segment.outcome], vars["LABEL"], [], vars["GROUP"])
         # If the range is predefined, use predefined transformation function
@@ -281,12 +230,12 @@ class DefaultImputationPreprocessor(Preprocessor):
             Preprocessed data.
         """
         logging.info("Preprocessor static features.")
-        data = {step: self.process_dynamic_data(data[step], vars) for step in data}
+        data = {step: self._process_dynamic_data(data[step], vars) for step in data}
 
         dyn_rec = Recipe(data[Split.train][Segment.dynamic], [], vars[Segment.dynamic], vars["GROUP"], vars["SEQUENCE"])
         if self.scaling:
             dyn_rec.add_step(StepScale())
-        data = self.apply_recipe_to_splits(dyn_rec, data, Segment.dynamic)
+        data = apply_recipe_to_splits(dyn_rec, data, Segment.dynamic)
 
         data[Split.train][Segment.features] = (
             data[Split.train].pop(Segment.dynamic).loc[:, vars[Segment.dynamic] + [vars["GROUP"], vars["SEQUENCE"]]]
@@ -302,27 +251,27 @@ class DefaultImputationPreprocessor(Preprocessor):
     def to_cache_string(self):
         return super().to_cache_string() + f"_imputation_{self.use_static_features}_{self.scaling}"
 
-    @staticmethod
-    def apply_recipe_to_splits(recipe: Recipe, data: dict[dict[pd.DataFrame]], type: str) -> dict[dict[pd.DataFrame]]:
-        """Fits and transforms the training features, then transforms the validation and test features with the recipe.
-
-        Args:
-            recipe: Object containing info about the features and steps.
-            data: Dict containing 'train', 'val', and 'test' and types of features per split.
-            type: Whether to apply recipe to dynamic features, static features or outcomes.
-
-        Returns:
-            Transformed features divided into 'train', 'val', and 'test'.
-        """
-        data[Split.train][type] = recipe.prep()
-        data[Split.val][type] = recipe.bake(data[Split.val][type])
-        data[Split.test][type] = recipe.bake(data[Split.test][type])
-        return data
-
-    def process_dynamic_data(self, data, vars):
+    def _process_dynamic_data(self, data, vars):
         if self.filter_missing_values:
             rows_to_remove = data[Segment.dynamic][vars[Segment.dynamic]].isna().sum(axis=1) != 0
             ids_to_remove = data[Segment.dynamic].loc[rows_to_remove][vars["GROUP"]].unique()
             data = {table_name: table.loc[~table[vars["GROUP"]].isin(ids_to_remove)] for table_name, table in data.items()}
             logging.info(f"Removed {len(ids_to_remove)} stays with missing values.")
         return data
+
+@staticmethod
+def apply_recipe_to_splits(recipe: Recipe, data: dict[dict[pd.DataFrame]], type: str) -> dict[dict[pd.DataFrame]]:
+    """Fits and transforms the training features, then transforms the validation and test features with the recipe.
+
+    Args:
+        recipe: Object containing info about the features and steps.
+        data: Dict containing 'train', 'val', and 'test' and types of features per split.
+        type: Whether to apply recipe to dynamic features, static features or outcomes.
+
+    Returns:
+        Transformed features divided into 'train', 'val', and 'test'.
+    """
+    data[Split.train][type] = recipe.prep()
+    data[Split.val][type] = recipe.bake(data[Split.val][type])
+    data[Split.test][type] = recipe.bake(data[Split.test][type])
+    return data
