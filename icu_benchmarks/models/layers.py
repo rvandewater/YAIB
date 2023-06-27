7c3d5111a5af1b5af7300e380df1354a1aa3dfae
youssef mecky <youssefmecky@pc-79-109.customer.ask4.lan>
2023-06-27 14:18:19 +0200

    removed quantile loss

--- icu_benchmarks/models/layers.py
+++ icu_benchmarks/models/layers.py
@@ -758,14 +758,3 @@ class TFTBack(nn.Module):
         out = self.quantile_proj(x)
 
         return out
-@gin.configurable
-class QuantileLoss(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.register_buffer('q', torch.tensor(config.quantiles))
-
-    def forward(self, predictions, targets):
-        diff = predictions - targets
-        ql = (1-self.q)*F.relu(diff) + self.q*F.relu(-diff)
-        losses = ql.view(-1, ql.shape[-1]).mean(0)
-        return losses
