da9725bbec1614d475d5dccd235e2fdc57504fc1
youssef mecky <youssefmecky@pc-79-109.customer.ask4.lan>
2023-06-27 14:18:09 +0200

    added quantile loss

--- icu_benchmarks/models/wrappers.py
+++ icu_benchmarks/models/wrappers.py
@@ -28,7 +28,17 @@ gin.config.external_configurable(torch.nn.functional.mse_loss, module="torch.nn.
 gin.config.external_configurable(sklearn.metrics.mean_squared_error, module="sklearn.metrics")
 gin.config.external_configurable(sklearn.metrics.log_loss, module="sklearn.metrics")
 
+@gin.configurable
+class QuantileLoss(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.register_buffer('q', torch.tensor(config.quantiles))
 
+    def forward(self, predictions, targets):
+        diff = predictions - targets
+        ql = (1-self.q)*F.relu(diff) + self.q*F.relu(-diff)
+        losses = ql.view(-1, ql.shape[-1]).mean(0)
+        return losses
 @gin.configurable("BaseModule")
 class BaseModule(LightningModule):
     needs_training = False
