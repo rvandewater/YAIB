5b97aacef67efe35089036edbb693e9eca05c476
youssef mecky <youssefmecky@youssefs-MacBook-Air.local>
2023-07-04 15:56:27 +0200

    changed gin file

--- icu_benchmarks/models/layers.py
+++ icu_benchmarks/models/layers.py
@@ -778,14 +778,3 @@ class TFTBack(nn.Module):
         out = self.quantile_proj(x)
 
         return out
-@gin.configurable
-class QuantileLoss(torch.nn.Module):
-    def __init__(self, quantiles):
-        super().__init__()
-        self.register_buffer('q', torch.tensor(quantiles))
-
-    def forward(self, predictions, targets):
-        diff = predictions - targets
-        ql = (1-self.q)*F.relu(diff) + self.q*F.relu(-diff)
-        losses = ql.view(-1, ql.shape[-1]).mean(0)
-        return losses
