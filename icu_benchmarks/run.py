3c9b729a4f559b2ab584728a12217602aefc8e19
youssef mecky <youssefmecky@youssefs-MacBook-Air.local>
2023-09-12 15:33:41 +0200

    reproducible forced to be there for evaulate

--- icu_benchmarks/run.py
+++ icu_benchmarks/run.py
@@ -9,7 +9,11 @@ import importlib.util
 
 import torch.cuda
 
-from icu_benchmarks.wandb_utils import update_wandb_config, apply_wandb_sweep, set_wandb_run_name
+from icu_benchmarks.wandb_utils import (
+    update_wandb_config,
+    apply_wandb_sweep,
+    set_wandb_run_name,
+)
 from icu_benchmarks.tuning.hyperparameters import choose_and_bind_hyperparameters
 from scripts.plotting.utils import plot_aggregated_results
 from icu_benchmarks.cross_validation import execute_repeated_cv
@@ -45,6 +49,7 @@ def main(my_args=tuple(sys.argv[1:])):
     setup_logging(date_format, log_format, verbose)
 
     # Load weights if in evaluation mode
+
     load_weights = args.command == "evaluate"
     data_dir = Path(args.data_dir)
 
@@ -52,7 +57,8 @@ def main(my_args=tuple(sys.argv[1:])):
     name = args.name
     task = args.task
     model = args.model
-    reproducible = args.reproducible
+
+    reproducible = args.reproducible if args.command != "evaluate" else False
 
     # Set experiment name
     if name is None:
@@ -71,7 +77,9 @@ def main(my_args=tuple(sys.argv[1:])):
     logging.info(f"Task mode: {mode}.")
     experiment = args.experiment
 
-    pretrained_imputation_model = load_pretrained_imputation_model(args.pretrained_imputation)
+    pretrained_imputation_model = load_pretrained_imputation_model(
+        args.pretrained_imputation
+    )
 
     # Log imputation model to wandb
     update_wandb_config(
@@ -86,29 +94,43 @@ def main(my_args=tuple(sys.argv[1:])):
     log_dir = (
         (log_dir_name / experiment)
         if experiment
-        else (log_dir_name / (args.task_name if args.task_name is not None else args.task) / model)
+        else (
+            log_dir_name
+            / (args.task_name if args.task_name is not None else args.task)
+            / model
+        )
     )
     if torch.cuda.is_available():
         for name in range(0, torch.cuda.device_count()):
-            log_full_line(f"Available GPU {name}: {torch.cuda.get_device_name(name)}", level=logging.INFO)
+            log_full_line(
+                f"Available GPU {name}: {torch.cuda.get_device_name(name)}",
+                level=logging.INFO,
+            )
     else:
         log_full_line(
-            "No GPUs available: please check your device and Torch,Cuda installation if unintended.", level=logging.WARNING
+            "No GPUs available: please check your device and Torch,Cuda installation if unintended.",
+            level=logging.WARNING,
         )
 
     log_full_line(f"Logging to {log_dir}.", logging.INFO)
 
     if args.preprocessor:
         # Import custom supplied preprocessor
-        log_full_line(f"Importing custom preprocessor from {args.preprocessor}.", logging.INFO)
+        log_full_line(
+            f"Importing custom preprocessor from {args.preprocessor}.", logging.INFO
+        )
         try:
-            spec = importlib.util.spec_from_file_location("CustomPreprocessor", args.preprocessor)
+            spec = importlib.util.spec_from_file_location(
+                "CustomPreprocessor", args.preprocessor
+            )
             module = importlib.util.module_from_spec(spec)
             sys.modules["preprocessor"] = module
             spec.loader.exec_module(module)
             gin.bind_parameter("preprocess.preprocessor", module.CustomPreprocessor)
         except Exception as e:
-            logging.error(f"Could not import custom preprocessor from {args.preprocessor}: {e}")
+            logging.error(
+                f"Could not import custom preprocessor from {args.preprocessor}: {e}"
+            )
 
     if load_weights:
         # Evaluate
@@ -120,14 +142,22 @@ def main(my_args=tuple(sys.argv[1:])):
         # Train
         checkpoint = log_dir / args.checkpoint if args.checkpoint else None
         model_path = (
-            Path("configs") / ("imputation_models" if mode == RunMode.imputation else "prediction_models") / f"{model}.gin"
+            Path("configs")
+            / (
+                "imputation_models"
+                if mode == RunMode.imputation
+                else "prediction_models"
+            )
+            / f"{model}.gin"
         )
         gin_config_files = (
             [Path(f"configs/experiments/{args.experiment}.gin")]
             if args.experiment
             else [model_path, Path(f"configs/tasks/{task}.gin")]
         )
-        gin.parse_config_files_and_bindings(gin_config_files, args.hyperparams, finalize_config=False)
+        gin.parse_config_files_and_bindings(
+            gin_config_files, args.hyperparams, finalize_config=False
+        )
         log_full_line(f"Data directory: {data_dir.resolve()}", level=logging.INFO)
         run_dir = create_run_dir(log_dir)
         choose_and_bind_hyperparameters(
