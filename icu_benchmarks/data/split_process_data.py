84273532c96f563329da09cf8a27673e70abc3cc
Robin van de Water <r.p.vandewater@gmail.com>
2025-06-04 10:22:37 +0200

    ruff ci

--- icu_benchmarks/data/split_process_data.py
+++ icu_benchmarks/data/split_process_data.py
@@ -126,7 +126,7 @@ def preprocess_data(
     logging.info(f"Loaded data: {list(data.keys())}")
     data = check_sanitize_data(data, vars)
 
-    if not (Segment.dynamic in data.keys()):
+    if (Segment.dynamic not in data.keys()):
         logging.warning("No dynamic data found, using only static data.")
 
     logging.debug(f"Modality mapping: {modality_mapping}")
