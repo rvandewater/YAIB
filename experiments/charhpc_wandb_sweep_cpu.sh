8af76b17a184a8f6756ead9b9995cc27c521afd8
Robin van de Water <r.p.vandewater@gmail.com>
2025-08-25 11:38:47 +0200

    more time for running experiments

--- experiments/charhpc_wandb_sweep_cpu.sh
+++ experiments/charhpc_wandb_sweep_cpu.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=yaib_experiment
 #SBATCH --partition=compute # -p
 #SBATCH --cpus-per-task=16 # -c
-#SBATCH --mem=250gb
+#SBATCH --mem=300gb
 #SBATCH --output=logs/classification_%a_%j.log # %j is job id
 #SBATCH --time=48:00:00
 
