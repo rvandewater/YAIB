d561303575f303b792e07778d8ce398bef15f51e
Robin van de Water <r.p.vandewater@gmail.com>
2026-06-03 11:21:06 +0200

    remove version

--- icu_benchmarks/__init__.py
+++ icu_benchmarks/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Top-level package for YAIB."""
+"""Top-level package for Yet Another ICU Benchmark (YAIB)."""
 
 __author__ = "Robin van de Water"
 __email__ = "robin.vandewater@hpi.de"
-__version__ = "0.1.0"
